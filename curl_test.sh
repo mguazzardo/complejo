a3efd27787b95850c99a0d7aea0403e0edeb51a9
mguazzardo <mguazzardo76@gmail.com>
2020-07-30 01:21:24 +0000

    subir curl_test

---
+++ curl_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#url="172.17.0.5"
+url=$(docker inspect apache | grep IPA | tail -1 | awk -F\" '{print $4}')
+status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url)
+date=`date`
+
+if [ $status_code -eq "200" ]
+then
+        echo "todo ok $date"
+else
+	echo "fallo $date"
+	// tirar codigo de error, exit 1
+	exit 1
+fi
